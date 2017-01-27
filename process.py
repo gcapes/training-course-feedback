bc14f5df15f73adfeba2c24e9ef8a3de7c03ce4c
Gerard Capes <gerard.capes@manchester.ac.uk>
2017-01-27 08:56:39 +0000

    Use command line argument for input file

--- process.py
+++ process.py
@@ -5,10 +5,15 @@ Created on Thu Jan 26 14:18:52 2017
 
 @author: mbexegc2
 """
-
+import sys
 import csv
 
-csvfile=open('/home/mbexegc2/Downloads/Research IT course application.csv')
+# Accept command line arguments for flexibility of file name and location
+# Check there is only one argument
+assert len(sys.argv)==2, "Wrong number of command line arguments"
+
+inputfile=sys.argv[1]
+csvfile=open(inputfile)
 contents=csv.reader(csvfile)
 
 # Assert to check which column headers respond to which column number
