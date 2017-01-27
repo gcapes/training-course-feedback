90e2d69c9145bea072fccebb1838b19bf9d590aa
Gerard Capes <gerard.capes@manchester.ac.uk>
2017-01-27 09:33:44 +0000

    Add assertions for column headers

--- process.py
+++ process.py
@@ -14,12 +14,20 @@ assert len(sys.argv)==2, "Wrong number of command line arguments"
 
 inputfile=sys.argv[1]
 csvfile=open(inputfile)
-contents=csv.reader(csvfile)
+contents=list(csv.reader(csvfile))
 
-# Assert to check which column headers respond to which column number
-contentlist=list(contents)
-print(contentlist[0][8])
+# Check column headers of interest haven't changed
+timecol=0
+emailcol=2
+coursecol=3
+vcscol=8
+assert contents[0][timecol]=="Timestamp"
+assert contents[0][emailcol]=="University Of Manchester email address"
+assert contents[0][coursecol]=='Which course are you applying to?'
+assert contents[0][vcscol]=='Which version control systems do you use?'
 
 for row in contentlist:
-    if row[7]=='None' and row[4]!='Version control with Git and GitHub':
+    if row[vcscol]=='None' and row[coursecol]!='Version control with Git and GitHub':
         print(row[2])
+
+# Check for duplicates
