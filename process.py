5c4a1c5dcb9bf5dece644a5fd448ac03e4071cb5
Gerard Capes <gerard.capes@manchester.ac.uk>
2017-01-27 13:45:59 +0000

    Extract emails for git course promotion
    Assert there are two command line arguments:
    	1. input file
    	2. archive file
    Process only new data.
    Update archive file.

--- process.py
+++ process.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 26 14:18:52 2017
-
-@author: mbexegc2
-"""
+# Script to process applications to moderated Research IT courses
+    # Download the responses to the google forms questionnaire
+    # Call the script with two arguments, the csv file and the archived responses
+    # Script returns a list of people who don't use version control
+    # Email them to advertise Git course
 import sys
+import os
 import csv
+import shutil
 
 # Accept command line arguments for flexibility of file name and location
-# Check there is only one argument
-assert len(sys.argv)==2, "Wrong number of command line arguments"
+# Check there are two arguments
+assert len(sys.argv)==3, "Two arguments required: <currentfile.csv> <archivefile.csv>"
 
 inputfile=sys.argv[1]
 csvfile=open(inputfile)
@@ -26,8 +26,31 @@ assert contents[0][emailcol]=="University Of Manchester email address"
 assert contents[0][coursecol]=='Which course are you applying to?'
 assert contents[0][vcscol]=='Which version control systems do you use?'
 
-for row in contentlist:
-    if row[vcscol]=='None' and row[coursecol]!='Version control with Git and GitHub':
-        print(row[2])
+# Check for updates to file
+archivefilename=sys.argv[2]
+if os.path.isfile(archivefilename):
+    # Archive file exists
+    archivefile=open(archivefilename)
+    archivedata=list(csv.reader(archivefile))
+    if len(archivedata)<len(contents):
+        # There is new data to process
+        startrow=len(archivedata)
+else:
+    # Archive file doesn't exist, so process all data
+    startrow=0
 
-# Check for duplicates
+needsgit=[]
+
+# If there is data to process, startrow should exist
+if 'startrow' in locals():
+    for row in contents[startrow:]:
+        if row[vcscol]=='None' and row[coursecol]!='Version control with Git and GitHub':
+            needsgit.append(row[2])
+    # Check for duplicates
+    needsgit=set(needsgit)
+    print(list(needsgit))
+    # Save email addresses to file
+    emails=csv.writer(open('emailgitpromo.csv','w'))
+    emails.writerow(list(needsgit))
+    # Overwrite archive with current file
+    shutil.copyfile(inputfile,archivefilename)
