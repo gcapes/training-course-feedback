cad4064f2325c0b1f82a5936de8683722c35a7fb
Gerard Capes <gerard.capes@manchester.ac.uk>
2017-02-06 13:01:52 +0000

    Enable use as module, fix bugs revealed
    Mainly using global variables within functions.
    If applicant has multiple responses, and one of them indicates use
    of VCS, don't email them.
    Previous logic omitted to check this possibility.

--- process.py
+++ process.py
@@ -10,32 +10,12 @@ import os
 import csv
 import shutil
 
-# Check there are three arguments
-assert len(sys.argv)==4, ("Three arguments required: "
-	"<currentresponses.csv> <archive.csv> <gitpromolist.csv>")
-
-inputfile=sys.argv[1]
-archivefilename=sys.argv[2]
-gitpromofile=sys.argv[3]
-
-csvfile=open(inputfile)
-contents=list(csv.reader(csvfile))
-
-# Check column headers of interest haven't changed
-timecol=0
-emailcol=2
-coursecol=3
-vcscol=8
-
 def check_headers(contents,timecol,emailcol,coursecol,vcscol):
     assert contents[0][timecol]=="Timestamp"
     assert contents[0][emailcol]=="University Of Manchester email address"
     assert contents[0][coursecol]=='Which course are you applying to?'
     assert contents[0][vcscol]=='Which version control systems do you use?'
 
-check_headers(contents,timecol,emailcol,coursecol,vcscol)
-
-# Check for updates to file
 def get_start_row(archivefilename):
     if os.path.isfile(archivefilename):
         # Archive file exists
@@ -49,12 +29,9 @@ def get_start_row(archivefilename):
 
     return startrow 
 
-startrow=get_start_row(archivefilename)
-
-# Get list of email addresses for Git course promotion
-def get_emails(responsedata,vcscol,emailcol):
+def get_emails(responsedata,vcscol,emailcol,coursecol,startrow):
     needsgit=[]
-    for row in contents[startrow:]:
+    for row in responsedata[startrow:]:
         if row[vcscol]=='None' \
             and row[coursecol]!='Version control with Git and GitHub'\
             and '@' in row[emailcol]: # Check email address has been entered
@@ -65,21 +42,45 @@ def get_emails(responsedata,vcscol,emailcol):
 
         # Now process the needsgit list to remove entries with a subsequent
         # application to the Git course
-        for row in contents[startrow:]:
-            if row[coursecol]=='Version control with Git and GitHub'\
-                and row[emailcol] in needsgit:
+        for row in responsedata[startrow:]:
+            if (row[coursecol]=='Version control with Git and GitHub'\
+                or row[vcscol]!='None') and row[emailcol] in needsgit:
                 needsgit.remove(row[emailcol])
     return needsgit
 
-needsgit=get_emails(contents,vcscol,emailcol)
-
-# Whether or not there are any matches, print output and update files.
-# This enables use of a makefile.
-print('%i people for Git course promotion' % len(needsgit))    
-
-# Save email addresses to file
-emails=csv.writer(open(gitpromofile,'w'))
-emails.writerow(list(needsgit))
-
-# Overwrite archive with current file
-shutil.copyfile(inputfile,archivefilename)
+# Run the following commands if executed as a script. Ignore if imported as module.
+if __name__=="__main__":
+    # Check there are three arguments
+    assert len(sys.argv)==4, ("Three arguments required: "
+    	"<currentresponses.csv> <archive.csv> <gitpromolist.csv>")
+    
+    inputfile=sys.argv[1]
+    archivefilename=sys.argv[2]
+    gitpromofile=sys.argv[3]
+    
+    csvfile=open(inputfile)
+    contents=list(csv.reader(csvfile))
+    
+    # Check column headers of interest haven't changed
+    timecol=0
+    emailcol=2
+    coursecol=3
+    vcscol=8
+    check_headers(contents,timecol,emailcol,coursecol,vcscol)
+    
+    # Check for updates to file
+    startrow=get_start_row(archivefilename)
+    
+    # Get list of email addresses for Git course promotion
+    needsgit=get_emails(contents,vcscol,emailcol,coursecol,startrow)
+    
+    # Whether or not there are any matches, print output and update files.
+    # This enables use of a makefile.
+    print('%i people for Git course promotion' % len(needsgit))    
+    
+    # Save email addresses to file
+    emails=csv.writer(open(gitpromofile,'w'))
+    emails.writerow(list(needsgit))
+    
+    # Overwrite archive with current file
+    shutil.copyfile(inputfile,archivefilename)
