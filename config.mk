7905fa1cd41277f243d4646f5af80ebb4c5c2868
Gerard Capes <gerard.capes@manchester.ac.uk>
2017-02-08 15:48:41 +0000

    Update config.mk to use new scripts
    (Even though the scripts are not complete yet)

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # Variables
-GIT_SOURCE=process.py
+GIT_SOURCE=processfeedback.py
 GIT_EXE=python $(GIT_SOURCE)
 RESPONSES=responses.csv
 GIT_EMAIL=emailgitpromo.csv
-TEST_GIT_SOURCE=testgit.py
+TEST_GIT_SOURCE=testfeedback.py
 TEST_GIT_EXE=python $(TEST_GIT_SOURCE)
