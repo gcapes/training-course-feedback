b18a2a7facbfa0356323def9fd2b246a4b81961f
Gerard Capes <gerard.capes@manchester.ac.uk>
2017-06-20 11:11:24 +0100

    Add test using gitpromoarchive

--- testfeedback.py
+++ testfeedback.py
@@ -14,6 +14,7 @@ import processfeedback
     # 7-8 also use the git course attendance data
 # 7. Attended Automation and Make, doesn't use version control, applied to Git course. Result: not on list.
 # 8. Attended UNIX shell, doesn't use version control, hasn't applied to Git course. Result: on list.
+# 9. Attended UNIX shell, doesn't use version control, hasn't applied to Git course, but already emailed. Result: not on list.
 testdata=[
 ['14/10/2016 16:41:59', 'Scenario 1', 'scenario1@manchester.ac.uk', 'Typesetting your thesis with LaTeX', 'Word of mouth', 'use Latex', 'Humanities', 'None', 'None', 'None'], 
 ['15/10/2016 15:52:37', 'Scenario 1 Repeat', 'scenario1.repeat@manchester.ac.uk', 'Typesetting your thesis with LaTeX', "Thomas Bishop's mouth", 'er typesetting with LaTeX', 'Humanities', 'R', 'None', 'None'], 
@@ -26,7 +27,9 @@ testdata=[
 ['18/10/2016 08:45:26', 'Scenario 6', 'scenario6@manchester.ac.uk', 'Programming in MATLAB', 'Word of mouth', 'How to do scripts with MATLAB.', 'Science and Engineering', 'C/C++', 'None', 'Use of sub-programs (e.g. functions, subroutines, methods, procedures)'], 
 ['18/10/2016 08:48:19', 'Scenario 6', 'scenario6@manchester.ac.uk', 'Programming in Python', 'Staffnet (staffnet.manchester.ac.uk/employment/training/it-systems/research-computing/research-courses/)', 'Research', 'Humanities', 'C/C++', 'SVN', 'Use of sub-programs (e.g. functions, subroutines, methods, procedures)'],
 ['','Scenario 7','scenario7@manchester.ac.uk','Automation and Make','','','','','None'],
-['','Scenario 8','scenario8@manchester.ac.uk','UNIX shell','','','','','None']]
+['','Scenario 8','scenario8@manchester.ac.uk','UNIX shell','','','','','None'],
+['','Scenario 9','scenario9@manchester.ac.uk','UNIX shell','','','','','None']
+]
 vcscol=8
 emailcol=2
 coursecol=3
