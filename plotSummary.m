54c4debf53c1dec4eef1f4dd4ce788696c38f98e
Gerard Capes <gerard.capes@manchester.ac.uk>
2017-11-05 13:33:41 +0000

    Add script for plotting summary bar chart

---
+++ plotSummary.m
@@ -0,0 +1,18 @@
+% Split VCS responses
+vcs = splitMulti(feedback,'vcs');
+
+% Generate summary statistics
+vcsCounts = histcounts(vcs.vcs(vcs.faculty == 'EPS'));
+vcsCounts(2,:)=histcounts(vcs.vcs(vcs.faculty=='BMH'));
+vcsCounts(3,:)=histcounts(vcs.vcs(vcs.faculty=='Hum'));
+vcsCounts(4,:)=histcounts(vcs.vcs(vcs.faculty=='PSS'));
+
+% Transpose for bar plot
+vcsCounts = vcsCounts.';
+vcsCats = categorical(categories(vcs.vcs));
+
+% Plot results
+bar(vcsCats,vcsCounts,'stacked')
+legend('EPS','BMH','Hum','PSS')
+title('VCS usage by faculty')
+ylabel('Frequency')
