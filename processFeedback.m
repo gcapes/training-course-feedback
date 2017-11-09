fb17512514a3b8b66d1ccd4fa2c2d820a4fc60ef
Gerard Capes <gerard.capes@manchester.ac.uk>
2017-11-09 13:46:05 +0000

    Update main script to process software engineering

--- processFeedback.m
+++ processFeedback.m
@@ -1,6 +1,11 @@
+% Load responses to RIT training course feedback questionnaire
+% Clean and process data
+% Make summary plots
+
 loadFeedback
 cleanCategories
 getStaffStudent
 % Split VCS responses
-vcs = splitMulti(feedback,'vcs');
+vcs = splitMulti(feedback,'vcs',{'None','Git','Subversion','Mercurial'});
 plotSummary
+softEng = splitMulti(feedback,'softEng',{'None','Comments','Subprograms',' Assertions','Unit testing','Code reviews','Defensive programming'});
