6994b6152dc8a565121fe5f3aa1dd039a2bc86f3
Gerard Capes <gerard.capes@manchester.ac.uk>
2021-10-28 11:54:36 +0100

    Use new file name for google app client secret

--- analysis/download_feedback.py
+++ analysis/download_feedback.py
@@ -29,7 +29,7 @@ def download_feedback():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                'client_secret.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
